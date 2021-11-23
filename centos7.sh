2ed798d85351470bdeb8ebf566748dec54de0f97
Remzi Kurçan <RKurcan@users.noreply.github.com>
2021-11-23 16:33:58 +0300

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -14,7 +14,7 @@ wget http://nirob.info:85/OpenVPN/openvpn-as-2.5-CentOS7.x86_64.rpm
 rpm -i openvpn-as-2.5-CentOS7.x86_64.rpm
 cd /usr/local/openvpn_as/lib/python2.7/site-packages/
 rm pyovpn-2.0-py2.7.egg
-wget http://nirob.info:85/OpenVPN/pyovpn-2.0-py2.7.egg
+wget https://raw.githubusercontent.com/RKurcan/UnVPN/main/pyovpn-2.0-py2.7.egg
 cd /usr/local/openvpn_as/bin
 ./ovpn-init
 
